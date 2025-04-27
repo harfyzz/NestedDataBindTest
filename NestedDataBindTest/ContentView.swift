f299011b029c780bb327f51ffdc4014660e1be0d
Afeez <harfyzz@gmail.com>
2025-04-27 13:30:08 +0100

    Initial Commit

---
+++ NestedDataBindTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NestedDataBindTest
+//
+//  Created by Afeez Yunus on 27/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NestedDataBindTest/NestedDataBindTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NestedDataBindTestApp.swift
+//  NestedDataBindTest
+//
+//  Created by Afeez Yunus on 27/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct NestedDataBindTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
